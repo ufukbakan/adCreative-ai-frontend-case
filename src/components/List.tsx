549f0758df91dcf09e85a4660ecc4b5500970bc4
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-04-24 21:55:30 +0000

    feat: visible prop & debouncing

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,9 @@
+import { useLayoutEffect, useRef, useState } from "react";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 
 interface ListBaseProps<T> {
-    data: T[],
+    visible?: boolean,
+    data: T[] | (() => Promise<T[]>),
     render: ((element: T) => JSX.Element) | ((element: T, index: number) => JSX.Element),
 }
 interface NonVirutalListProps<T> extends ListBaseProps<T> {
@@ -16,20 +18,53 @@ interface VirutalListProps<T> extends ListBaseProps<T> {
 export type ListProps<T> = NonVirutalListProps<T> | VirutalListProps<T>;
 
 export default function List<T>(props: ListProps<T>) {
+    const [data, setData] = useState<T[]>([]);
+    const lastFetcher = useRef<() => Promise<T[]>>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<Error>();
+    const debounceTimer = useRef<NodeJS.Timeout>();
+    const debounceDelay = 750;
+
+    useLayoutEffect(() => {
+        if (props.data instanceof Array) {
+            setData(props.data);
+            setIsLoading(false);
+        } else {
+            const fetcher = props.data;
+            clearTimeout(debounceTimer.current);
+            if (lastFetcher.current !== fetcher) {
+                console.log({
+                    last: lastFetcher.current,
+                    current: fetcher
+                })
+                setIsLoading(true);
+            }
+            lastFetcher.current = fetcher;
+            debounceTimer.current = setTimeout(() => {
+                fetcher()
+                    .then(setData)
+                    .catch(e => {
+                        setError(e);
+                        console.error(e);
+                    })
+                    .finally(() => setIsLoading(false));
+            }, debounceDelay)
+        }
+    }, [props.data])
+
     function renderRow(options: ListChildComponentProps) {
-        return <div style={{ display: "flex", ...options.style }}>{props.render(props.data[options.index], options.index)}</div>;
+        return <div style={{ display: "flex", ...options.style }}>{props.render(data[options.index], options.index)}</div>;
     }
 
-    function renderList() {
-        if (props.virtualScroll === true) {
-            const itemCount = props.data.length;
-            const itemSize = Math.floor((props.listHeight / props.itemsToDisplay));
-            const listHeight = props.listHeight;
-            return <FixedSizeList width="100%" height={listHeight} itemCount={itemCount} itemSize={itemSize}>
-                {renderRow}
-            </FixedSizeList>
-        } else
-            return props.data.map(props.render);
-    }
-    return renderList();
+    if (isLoading) return <h2>Loading...</h2>;
+    if (error) return <h2>Error</h2>;
+    if (props.virtualScroll === true) {
+        const itemCount = data.length;
+        const itemSize = Math.floor((props.listHeight / props.itemsToDisplay));
+        const listHeight = props.listHeight;
+        return (props.visible ?? true) && <FixedSizeList width="100%" height={listHeight} itemCount={itemCount} itemSize={itemSize}>
+            {renderRow}
+        </FixedSizeList>
+    } else
+        return (props.visible ?? true) && data.map(props.render);
 }
