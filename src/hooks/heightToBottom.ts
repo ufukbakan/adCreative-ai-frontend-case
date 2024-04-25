4ec06b2a458d71ba1c7a41103c6d36812a5ce5af
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-04-25 12:27:23 +0300

    feat: add resize listener

--- src/hooks/heightToBottom.ts
+++ src/hooks/heightToBottom.ts
@@ -1,6 +1,13 @@
-import { RefObject } from "react";
+import { RefObject, useEffect, useState } from "react";
 
 export function useHeightToBottomScreen(ref: RefObject<HTMLElement>){
-    const windowHeight = window.innerHeight;
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+    const resizeListener = () => {
+        setWindowHeight(window.innerHeight);
+    }
+    useEffect(() => {
+        window.addEventListener("resize", resizeListener)
+        return () => window.removeEventListener("resize", resizeListener);
+    }, []);
     return windowHeight - (ref.current?.getBoundingClientRect().bottom ?? 0);
 }
